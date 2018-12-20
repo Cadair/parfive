3897fdb2280c98033a3a3a4a877ed624ca8ca891
Stuart Mumford <stuart@cadair.com>
2018-12-20 14:03:58 +0000

    Move all the setup stuff to setup.cfg

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
 import setuptools
 
-setuptools.setup(
-    name="parfive",
-    version="0.1.1",
-    url="https://github.com/cadair/parfive",
-
-    author="Stuart Mumford",
-
-    description="A parallel file downloader using asyncio.",
-    long_description=open('README.rst').read(),
-
-    packages=setuptools.find_packages(),
-
-    install_requires=['aiohttp', 'tqdm'],
-    extras_require={'ftp': ['aioftp']},
-    python_requires=">=3.5",
-
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-)
+setuptools.setup()
