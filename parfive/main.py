eed972c0da5807c4e0d42e5daeb5c6bea479be77
Vishnunarayan K I <appukuttancr@gmail.com>
2019-07-18 22:43:03 +0530

    feat: add cli for parfive

---
+++ parfive/main.py
@@ -0,0 +1,23 @@
+import argparse
+from parfive import Downloader
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Parfive, the python asyncio based downloader')
+    parser.add_argument('urls', metavar='URLS', type=str, nargs='+',
+                        help='an integer for the accumulator')
+    parser.add_argument('--max-conn', type=int, default=5,
+                        help='Number of maximum connections')
+    parser.add_argument('--overwrite', action='store_const', const=True, default=False,
+                        help='Overwrite if the file exists')
+    parser.add_argument('--no-file-progress', action='store_const', const=False, default=True, dest='file_progress',
+                        help='Overwrite if the file exists')
+    parser.add_argument('--directory', type=str, default='./',
+                        help='Directory to which downloaded files are saved.')
+
+    args = parser.parse_args()
+
+    downloader = Downloader(max_conn=args.max_conn, file_progress=args.file_progress, overwrite=args.overwrite)
+    for url in args.urls:
+        downloader.enqueue_file(url, path=args.directory)
+    downloader.download()
