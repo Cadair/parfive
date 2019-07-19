3ccc766ce35884054b8b2368de70c39be10b9ea5
Vishnunarayan K I <appukuttancr@gmail.com>
2019-07-19 12:32:31 +0530

    chore: update help strings

--- parfive/main.py
+++ parfive/main.py
@@ -7,13 +7,13 @@ from parfive import Downloader
 def main():
     parser = argparse.ArgumentParser(description='Parfive, the python asyncio based downloader')
     parser.add_argument('urls', metavar='URLS', type=str, nargs='+',
-                        help='an integer for the accumulator')
+                        help='URLs of files to be downloaded.')
     parser.add_argument('--max-conn', type=int, default=5,
-                        help='Number of maximum connections')
+                        help='Number of maximum connections.')
     parser.add_argument('--overwrite', action='store_const', const=True, default=False,
-                        help='Overwrite if the file exists')
+                        help='Overwrite if the file exists.')
     parser.add_argument('--no-file-progress', action='store_const', const=False, default=True, dest='file_progress',
-                        help='Overwrite if the file exists')
+                        help='Overwrite if the file exists.')
     parser.add_argument('--directory', type=str, default='./',
                         help='Directory to which downloaded files are saved.')
 
