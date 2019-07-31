2a4d1e0716e39d50422a8d85f2f7973bab25bef3
Vishnunarayan K I <appukuttancr@gmail.com>
2019-07-31 11:57:18 +0530

    tests: add some tests

--- parfive/main.py
+++ parfive/main.py
@@ -5,6 +5,21 @@ from parfive import Downloader
 
 
 def main():
+    args = parse_args(sys.argv[1:])
+    downloader = Downloader(max_conn=args.max_conn, file_progress=not args.no_file_progress, overwrite=args.overwrite)
+    for url in args.urls:
+        downloader.enqueue_file(url, path=args.directory)
+    results = downloader.download()
+    for i in results:
+        print(i)
+
+    err_str = ''
+    for err in results.errors:
+        err_str += f'{err.url} \t {err.exception}\n'
+    if err_str:
+        sys.exit(err_str)
+
+def parse_args(args):
     parser = argparse.ArgumentParser(description='Parfive, the python asyncio based downloader')
     parser.add_argument('urls', metavar='URLS', type=str, nargs='+',
                         help='URLs of files to be downloaded.')
@@ -19,17 +34,5 @@ def main():
     parser.add_argument('--print-filenames', action='store_const', const=True, default=False, dest='print_filenames',
                         help='Print successfully downloaded files\'s names to stdout.')
 
-    args = parser.parse_args()
-
-    downloader = Downloader(max_conn=args.max_conn, file_progress=not args.no_file_progress, overwrite=args.overwrite)
-    for url in args.urls:
-        downloader.enqueue_file(url, path=args.directory)
-    results = downloader.download()
-    for i in results:
-        print(i)
-
-    err_str = ''
-    for err in results.errors:
-        err_str += f'{err.url} \t {err.exception}\n'
-    if err_str:
-        sys.exit(err_str)
+    args = parser.parse_args(args)
+    return args
