0d3e198a94ab030c855607610534ef6bb8ea5db6
Stuart Mumford <stuart@cadair.com>
2020-08-17 10:48:07 +0100

    Packaging and docs updates

--- parfive/__init__.py
+++ parfive/__init__.py
@@ -1,16 +1,12 @@
 """parfive - A parallel file downloader using asyncio."""
-
-from pkg_resources import get_distribution, DistributionNotFound
-
 from .downloader import Downloader
 from .results import Results
 
 __all__ = ['Downloader', 'Results']
 
-__author__ = 'Stuart Mumford <stuart@cadair.com>'
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
+    from .version import __version__
+except ImportError:
+    print("version.py not found, please reinstall parfive.")
     __version__ = "unknown"
