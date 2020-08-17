81dd6995b55846a33c2a28fa36e7072bc6eabdb9
Stuart Mumford <stuart@cadair.com>
2020-08-17 10:48:07 +0100

    Packaging and docs updates

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+import os
 import setuptools
 
-setuptools.setup(use_scm_version=True)
+VERSION_TEMPLATE = """
+# Note that we need to fall back to the hard-coded version if either
+# setuptools_scm can't be imported or setuptools_scm can't determine the
+# version, so we catch the generic 'Exception'.
+try:
+    from setuptools_scm import get_version
+    __version__ = get_version(root='..', relative_to=__file__)
+except Exception:
+    __version__ = '{version}'
+""".lstrip()
+
+
+setuptools.setup(use_scm_version={'write_to': os.path.join('parfive', 'version.py'),
+                                  'write_to_template': VERSION_TEMPLATE})
