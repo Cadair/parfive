70891c9a91b1e37f6e84abdf6f90c9eeaa324598
David Stansby <dstansby@gmail.com>
2020-10-14 14:53:32 +0100

    Log download errors (#58)
    * Log download errors
    
    * Log at info level
    
    * Changelog
    
    * Fix changelog typo
    
    Co-authored-by: Stuart Mumford <stuart@cadair.com>
    
    Co-authored-by: Stuart Mumford <stuart@cadair.com>

--- parfive/__init__.py
+++ parfive/__init__.py
@@ -1,8 +1,9 @@
 """parfive - A parallel file downloader using asyncio."""
 from .downloader import Downloader
 from .results import Results
+from .utils import log
 
-__all__ = ['Downloader', 'Results']
+__all__ = ['Downloader', 'Results', 'log']
 
 
 try:
