1806adf3ef17e6311c3debe7df379443585821a1
Stuart Mumford <stuart@cadair.com>
2022-10-27 12:22:44 +0100

    Vendor better

--- parfive/utils.py
+++ parfive/utils.py
@@ -22,6 +22,19 @@ __all__ = [
 
 
 # Copied out of CPython under PSF Licence 2
+def _parseparam(s):
+    while s[:1] == ";":
+        s = s[1:]
+        end = s.find(";")
+        while end > 0 and (s.count('"', 0, end) - s.count('\\"', 0, end)) % 2:
+            end = s.find(";", end + 1)
+        if end < 0:
+            end = len(s)
+        f = s[:end]
+        yield f.strip()
+        s = s[end:]
+
+
 def parse_header(line):
     """Parse a Content-type like header.
     Return the main content-type and a dictionary of options.
