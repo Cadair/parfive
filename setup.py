250dcec99d0d398d513ddcce373dc940b4a6d392
CyclingNinja <bennett.sm89@gmail.com>
2024-02-07 16:49:32 +0000

    Experiment with cruft update

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
-from setuptools import setup
-
-setup()
+diff a/setup.py b/setup.py	(rejected hunks)
+@@ -1,7 +1,4 @@
+ #!/usr/bin/env python
+ from setuptools import setup
+ 
++setup()
